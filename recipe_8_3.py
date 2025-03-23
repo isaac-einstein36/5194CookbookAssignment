e5f0031f9a3180eb93b741084e09eee32392aebd
Isaac Einstein <einstein.15@osu.edu>
2025-03-23 17:52:30 -0400

    recipe 8.3 updates

--- recipe_8_3.py
+++ recipe_8_3.py
@@ -1,1 +1,23 @@
-print("Hello World")
+from picamera2 import Picamera2
+import cv2
+import time
+
+# Initialize the camera
+picam2 = Picamera2()
+config = picam2.create_preview_configuration(main={"size": (640,480)})
+picam2.configure(config)
+
+# Start the camera
+picam2.start()
+time.sleep(2)
+
+# capture the image
+img = picam2.capture_array()
+
+# display the image with OpenCV
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows
+
+# Stop the camera
+picam2.stop()
