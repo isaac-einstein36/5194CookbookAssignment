d6a9462b52933981d8e6a4da820e30b1e0f0c290
isaac-einstein36 <96673710+isaac-einstein36@users.noreply.github.com>
2025-03-24 11:50:37 -0400

    Update recipe_8_4.py

--- recipe_8_4.py
+++ recipe_8_4.py
@@ -3,22 +3,28 @@ from imutils.video import VideoStream
 from imutils import resize
 
 vs = VideoStream(src=0).start()
-old_count = 0
 
 while True:
-    try:
-        img = vs.read()
-        img = resize(img, width=800)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = cv2.blur(img, (3, 3)) 
+    img = vs.read()
+    img = resize(img, width=800)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.blur(img, (3, 3)) 
 
-        detected_circles = cv2.HoughCircles(img,  
-            cv2.HOUGH_GRADIENT, 1, 20, param1 = 50, 
-            param2 = 30, minRadius = 15, maxRadius = 100)
+    detected_circles = cv2.HoughCircles(img,  
+        cv2.HOUGH_GRADIENT, 1, 20, param1 = 50, 
+        param2 = 30, minRadius = 15, maxRadius = 100)
 
-        count = len(detected_circles[0]	)
-        if count != old_count:
-            print(count)
-            old_count = count
-    except:
-        vs.stop()
+    print(detected_circles)
+
+    for pt in detected_circles[0]: 
+        a, b, r = pt[0], pt[1], pt[2] 
+        print(a, b)
+        cv2.circle(img, (int(a), int(b)), int(r), (0, 0, 0), 2) 
+
+    cv2.imshow('image', img)
+    key = cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    if key == ord('x'):
+        break
+
+vs.stop()
